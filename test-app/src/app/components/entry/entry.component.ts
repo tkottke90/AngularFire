9d49fa85c60613ef685824a6a871adb3b53d2579
tkottke90 <altotom90@gmail.com>
2017-03-17 18:34:58 -0500

    Missed File in update

--- test-app/src/app/components/entry/entry.component.ts
+++ test-app/src/app/components/entry/entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-entry',
@@ -10,33 +10,37 @@ export class EntryComponent implements OnInit {
   isClassVisible:boolean =  true;
   isLoginVisible:boolean = true;
   userLoggedIn:boolean = false;
-
+ 
+  viewStatus: number;
   public myname: string;
-  @Input() viewStatus: number;
-  @Output() viewChange = new EventEmitter();
 
   constructor() {}
 
   ngOnInit() {
   }
 
-  ngOnChanges(changes){if(changes.value!=null){this.changeView}}
+  clearView(view: number){
+    this.changeView(view);
+    console.log("EntryComponent.clearView:" + view);
+  }
 
   changeView(view:number){
-    if(this.viewStatus = view){this.viewStatus = 0;}else{this.viewStatus = view;}
-    console.log(view);
-    console.log(this.viewStatus);
+    if(this.viewStatus == view){this.viewStatus = 0;}else{this.viewStatus = view;}
     switch(this.viewStatus){
       case 0: // Default
         this.isClassVisible = true;
         this.isLoginVisible = true;
+        break;
       case 1: // Sign Up User
         this.isClassVisible = false;
         this.isLoginVisible = true;
+        break;
       case 2: // Sign In User
         this.isClassVisible = true;
         this.isLoginVisible = false;
+        break;
       default: // Issue with value, set to 0 and rerun
+
         this.viewStatus = 0;
     }
   }
